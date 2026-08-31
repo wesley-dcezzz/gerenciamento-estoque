85640f37f7b54e100782360b92761ee37c236094
Wesley Souza <wesleyunivesp1701@gmail.com>
2026-08-31 13:13:53 -0300

    implementa rota de filtrar produtos por nome

--- Controllers/gerenciamentoController.cs
+++ Controllers/gerenciamentoController.cs
@@ -1,5 +1,6 @@
 ﻿using controleEstoque.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace controleEstoque.Controllers
@@ -20,7 +21,29 @@ namespace controleEstoque.Controllers
                 return BadRequest(new { sucesso = false, mensagem = "Dados inválidos." });
             }
 
-            return Ok(new { sucesso = true, mensagem = $"O Produto {request.nome} foi cadastrado com sucesso." });
+            return Created(string.Empty, new { sucesso = true, mensagem = $"O Produto {request.nome} foi cadastrado com sucesso." });
+        }
+
+        [HttpGet("filtrarNome")]
+        public async Task<IActionResult> FiltrarNome([FromQuery] string nome)
+        {
+            if (string.IsNullOrEmpty(nome))
+            {
+                return BadRequest(new { sucesso = false, mensagem = "Nome inválido." });
+            }
+
+            List<requestModel> produtosTeste = new List<requestModel>
+            {
+                new requestModel { nome = "Produto A", categoria = "Categoria 1", preco = "R$ 10,00", quantidade = 5 },
+                new requestModel { nome = "Produto A v2", categoria = "Categoria 1", preco = "R$ 12,00", quantidade = 10 },
+                new requestModel { nome = "Produto B", categoria = "Categoria 2", preco = "R$ 20,00", quantidade = 3 },
+                new requestModel { nome = "Produto C", categoria = "Categoria 1", preco = "R$ 15,00", quantidade = 8 }
+            };
+
+            // Filtra os produtos pelo nome
+            var produtosFiltrados = produtosTeste.Where(p => p.nome.Contains(nome, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            return Ok(new { sucesso = true, produtos = produtosFiltrados, mensagem = "Produtos filtrados com sucesso." });
         }
     }
 }
