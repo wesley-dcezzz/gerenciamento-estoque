80d5be2e84066b6d6c261417573f391a4ea29f0e
Wesley Souza <wesleyunivesp1701@gmail.com>
2026-08-31 17:26:05 -0300

    implementa endpoint para editar produto

--- Controllers/gerenciamentoController.cs
+++ Controllers/gerenciamentoController.cs
@@ -61,5 +61,36 @@ namespace controleEstoque.Controllers
 
             return Ok(new { sucesso = true, produtos = produtosFiltrados, mensagem = "Produtos filtrados com sucesso." });
         }
+
+        [HttpPut("editarProduto")]
+        public async Task<IActionResult> EditarProduto(int id, [FromBody] requestModel request)
+        {
+
+            //simulando lista de produtos sem ID
+            List<requestModel> produtosTeste = new List<requestModel>
+            {
+                new requestModel { nome = "Produto A", categoria = "Categoria 1", preco = "R$ 10,00", quantidade = 5 },
+                new requestModel { nome = "Produto A v2", categoria = "Categoria 1", preco = "R$ 12,00", quantidade = 10 },
+                new requestModel { nome = "Produto B", categoria = "Categoria 2", preco = "R$ 20,00", quantidade = 3 },
+                new requestModel { nome = "Produto C", categoria = "Categoria 1", preco = "R$ 15,00", quantidade = 8 }
+            };
+
+            //Como ID 1 corresponde ao índice 0 ( 1 - 1 =  0)
+            int indice = id - 1;
+
+            if (indice < 0 || indice >= produtosTeste.Count)
+            {
+                return NotFound(new { sucesso = false, mensagem = "Produto não encontrado." });
+            }
+
+            //atualiza os dados da lista com o que veio da Request
+            var produtoParaAtualizar = produtosTeste[indice];
+            produtoParaAtualizar.nome = request.nome;
+            produtoParaAtualizar.categoria = request.categoria;
+            produtoParaAtualizar.preco = request.preco;
+            produtoParaAtualizar.quantidade = request.quantidade;
+
+            return Ok(new { sucesso = true, produtoAtualizado = produtoParaAtualizar, mensagem = "Produto atualizado com sucesso."});
+        }
     }
 }
