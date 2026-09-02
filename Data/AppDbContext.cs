1d42ca36cf52da4fe7233a836b37dee1450a7c52
Wesley Souza <wesleyunivesp1701@gmail.com>
2026-09-02 17:39:00 -0300

    configuração banco de dados e entity

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using controleEstoque.Models;
+using controleEstoque.Entities;
+
+namespace controleEstoque.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Representa a tabela de produtos no banco de dados
+        public DbSet<ProdutoEntity> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configura a precisão do campo Preço (18 dígitos no total, sendo 2 casas decimais)
+            modelBuilder.Entity<ProdutoEntity>()
+                .Property(p => p.Preco)
+                .HasColumnType("decimal(18,2)");
+        }
+    }
+}
---
+++ Entities/ProdutoEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace controleEstoque.Entities
+{
+    public class ProdutoEntity
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public decimal Preco { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
