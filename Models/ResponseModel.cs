038965f24ce8f70981f640f09509e7208ed61e6f
Wesley Souza <wesleyunivesp1701@gmail.com>
2026-09-02 18:14:30 -0300

    criação service e response model

---
+++ Models/ResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace controleEstoque.Models
+{
+    public class ResponseModel
+    {
+        public string nome { get; set; }
+        public string categoria { get; set; }
+        public decimal preco { get; set; }
+        public int quantidade { get; set; }
+    }
+}
---
+++ Services/gerenciamentoService.cs
@@ -0,0 +1,7 @@
+﻿namespace controleEstoque.Services
+{
+    public class gerenciamentoService
+    {
+        
+    }
+}
